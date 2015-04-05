590c719385deddbd0ecbce231ae0cff6faf6a56e
Keen <kanary@inbox.ru>
2015-04-05 14:42:18 +0300

    Add host to logs

--- src/routes/log.js
+++ src/routes/log.js
@@ -1,6 +1,9 @@
 var log = require('../libs/log')(module);
 
 exports.warn = function(req, res) {
-	log.warn("New request at " + new Date() + " for req" + req.path + ". Message: " + req.body.msg);
+	log.warn("New request at " + new Date() +
+		" for req" + req.path +
+		". Host: " + req.host +
+		". Message: " + req.body.msg);
 	res.send('Ok');
 };
